8aeab45e681d4cb7abb7d80a62efd79e82125fb6
damianknysak <damian11rs@onet.pl>
2023-11-18 14:48:52 +0100

    tooltips-added

--- src/components/Home/RealEstateCard.tsx
+++ src/components/Home/RealEstateCard.tsx
@@ -3,6 +3,8 @@ import { BiExport } from "react-icons/bi";
 import { LuLayers, LuPencilLine } from "react-icons/lu";
 import { FaRegBuilding } from "react-icons/fa";
 import { RealEstateCardProps } from "./RealEstateList";
+import { Tooltip } from "@mui/material";
+import CustomTooltipButton from "./RealEstateCard/CustomTooltipButton";
 
 const RealEstateCard: React.FC<RealEstateCardProps> = ({
   title,
@@ -10,13 +12,11 @@ const RealEstateCard: React.FC<RealEstateCardProps> = ({
   address,
 }) => {
   return (
-    <article className="w-full h-64 bg-black rounded-lg p-2 border-2 border-secondary">
+    <article className="w-full h-64 bg-black rounded-lg p-2 border-2 border-secondary cursor-pointer">
       <div className="w-full h-32 bg-blue-500 rounded-lg"></div>
       <div className="flex justify-between mt-2 items-center">
         <span className=" text-white font-bold text-lg">{title}</span>
-        <button className="p-1.5 bg-secondary rounded-full transition-all duration-500 hover:bg-blue-gradient">
-          <IoShareSocialOutline color="white" size={20} />
-        </button>
+        <CustomTooltipButton title="Share" />
       </div>
       <div>
         <span className="text-gray-500 text-xs">{address}</span>
@@ -24,20 +24,14 @@ const RealEstateCard: React.FC<RealEstateCardProps> = ({
       <div className="border border-secondary my-2" />
       <div className="flex items-center justify-between">
         <div className="flex items-center space-x-2">
-          <button className="w-6 h-6 hover:bg-blue-gradient bg-secondary flex items-center justify-center rounded">
-            <FaRegBuilding color="white" />
-          </button>
-          <button className="w-6 h-6 hover:bg-blue-gradient bg-secondary flex items-center justify-center rounded">
-            <LuLayers color="white" />
-          </button>
-          <button className="w-6 h-6 hover:bg-blue-gradient bg-secondary flex items-center justify-center rounded">
-            <BiExport color="white" />
-          </button>
+          <CustomTooltipButton title="Building details" />
+
+          <CustomTooltipButton title="Layers" />
+
+          <CustomTooltipButton title="Export" />
         </div>
         <div className="flex items-center space-x-2">
-          <button className="w-6 h-6 hover:bg-blue-gradient bg-secondary flex items-center justify-center rounded">
-            <LuPencilLine color="white" />
-          </button>
+          <CustomTooltipButton title="Write to author" />
           <button className="h-6 px-2 hover:bg-blue-gradient space-x-1 bg-secondary flex items-center justify-center rounded">
             <IoLinkSharp color="white" />
             <span className="text-xs text-white">Visit site</span>
---
+++ src/components/Home/RealEstateCard/CustomTooltipButton.tsx
@@ -0,0 +1,40 @@
+import { Tooltip } from "@mui/material";
+import React from "react";
+import { BiExport } from "react-icons/bi";
+import { FaRegBuilding } from "react-icons/fa";
+import { IoShareSocialOutline } from "react-icons/io5";
+import { LuLayers, LuPencilLine } from "react-icons/lu";
+
+interface SmallButtonProps {
+  title: string;
+}
+
+const SmallButton = React.forwardRef<HTMLButtonElement, SmallButtonProps>(
+  ({ title, ...props }, ref) => {
+    return (
+      <button ref={ref} {...props}>
+        {title === "Write to author" && <LuPencilLine color="white" />}
+        {title === "Export" && <BiExport color="white" />}
+        {title === "Layers" && <LuLayers color="white" />}
+        {title === "Building details" && <FaRegBuilding color="white" />}
+        {title === "Share" && <IoShareSocialOutline color="white" size={20} />}
+      </button>
+    );
+  }
+);
+
+const CustomTooltipButton: React.FC<{ title: string }> = ({ title }) => {
+  return (
+    <div
+      className={`${
+        title === "Share" ? `w-8 h-8 rounded-full` : `w-6 h-6  rounded`
+      } hover:bg-blue-gradient bg-secondary flex items-center justify-center `}
+    >
+      <Tooltip title={title}>
+        <SmallButton title={title} />
+      </Tooltip>
+    </div>
+  );
+};
+
+export default CustomTooltipButton;
